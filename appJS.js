8c0d2365716ba4cced20913e15fc66e6b797645e
MRdevTagg <93151848+MRdevTagg@users.noreply.github.com>
2023-03-18 17:47:12 -0300

    ui

--- appJS.js
+++ appJS.js
@@ -472,13 +472,12 @@ $('#redo').addEventListener('click',(e)=>{
 
 function HandleSizes() {
 	return () => {
-		$('main').style['height'] = window.innerHeight + 'px';
-		$('.tools').style['top'] = visualViewport.height -$('.tools').offsetHeight-5 + 'px';
+		$('main').style['height'] = visualViewport.height + 'px';
 		$('#dialog').style['height'] = window.innerHeight + 'px';
 		[snippets_btn_container,download_btn,fileopen_btn].forEach(uielm => uielm.addCalls())
 
 	};
 }
-console.log()
+
 
 
